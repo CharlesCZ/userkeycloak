f9b7abe5975c9551feb78507ab6fdd65182f8bcd
czarek <karakankaczyn@gmail.com>
2019-08-16 23:38:33 +0200

    init

---
+++ userkeycloak/src/main/java/org/czekalski/userkeycloak/User.java
@@ -0,0 +1,11 @@
+package org.czekalski.userkeycloak;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public class User extends org.springframework.security.core.userdetails.User {
+    public User(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, authorities);
+    }
+}
---
+++ userkeycloak/src/main/java/org/czekalski/userkeycloak/UserkeycloakApplication.java
@@ -0,0 +1,13 @@
+package org.czekalski.userkeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserkeycloakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserkeycloakApplication.class, args);
+    }
+
+}
